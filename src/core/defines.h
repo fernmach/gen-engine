8d164dc84e1033170e2a4ca951b65c9f55b76c1c
fernmach <netfm5@gmail.com>
2025-07-14 15:29:00 -0300

    Moved the EntityId definition into a defines header to be shared acrros the project when necessary.

--- src/core/defines.h
+++ src/core/defines.h
@@ -10,9 +10,24 @@
 #ifndef _ENG_DEFNINES_H_
 #define _ENG_DEFNINES_H_
 
+//Forward declare SGDK
+typedef unsigned long u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
 /**
  *  \brief
- *      If used, forecs a function to be inlined.
+ *      Forward declaration helper when using EntityId
+ */
+#if ECS_MAX_ENTITIES > 255
+    typedef u16 EntityId;    
+#else
+    typedef u8 EntityId;
+#endif
+
+/**
+ *  \brief
+ *      If used, forces a function to be inlined.
  */
 #ifndef INLINE
 #define INLINE __attribute__((always_inline))
